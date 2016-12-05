f9b9a6738883678b7e3d71cdfb4d07a08c8b0db7
Yann <darkjador280@gmail.comgit config --global user.name Your>
2016-12-05 18:08:10 +0100

    olala

--- Tarot1/src/application/Main.java
+++ Tarot1/src/application/Main.java
@@ -1,10 +1,15 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
@@ -13,11 +18,37 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Group root = new Group();
-			Scene scene = new Scene(root,1080,800);
+			Scene scene = new Scene(root,1080,800,Color.BLUEVIOLET);
 			primaryStage.setTitle("Tarot tamer");
+			
+//			Image urlImage = new Image("Nocide.jpg");
+//			ImageView image = new ImageView();
+//			image.setImage(urlImage);
+//			image.setX(500);
+//			image.setY(400);
+//			image.setRotate(10);
+//			Group cartes = new Group();
+			
+			
+			Button dist = new Button();
+			dist.setLayoutX(50);
+			dist.setLayoutY(50);
+			dist.setText("Distribuer bordel");
+			dist.setOnAction(new EventHandler<ActionEvent>()
+					{
+
+						@Override
+						public void handle(ActionEvent event) {
+							System.out.println("lama");
+							
+						}
+						
+					});
+			root.getChildren().add(dist);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
